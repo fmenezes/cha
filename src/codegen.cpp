7528be7e980f0f147fe73ccb6135ee5e4e44b76e
Vinicius <vinperdom@gmail.com>
2022-01-15 03:11:45 -0300

    formatted document

--- src/codegen.cpp
+++ src/codegen.cpp
@@ -137,14 +137,13 @@ int ni::NProgram::codegen(std::string &error) const
         error = e.str();
         return 1;
     }
-    std::cout << "**** teste ****" << std::endl;
 
     std::vector<llvm::Type *> Args;
 
-     llvm::FunctionType *FT =
+    llvm::FunctionType *FT =
         llvm::FunctionType::get(llvm::Type::getVoidTy(*TheContext), Args, false);
 
-     llvm::Function *F =
+    llvm::Function *F =
         llvm::Function::Create(FT, llvm::Function::ExternalLinkage, 0, "main", TheModule);
 
     llvm::BasicBlock *BB = llvm::BasicBlock::Create(*TheContext, "entry", F);
