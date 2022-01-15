df40c4dee4da7f9f18cf2159e6c102cd978c8a07
Vinicius <vinperdom@gmail.com>
2022-01-15 03:23:07 -0300

    fixed rebase

--- src/codegen.cpp
+++ src/codegen.cpp
@@ -149,18 +149,7 @@ int ni::NProgram::codegen(std::string &error) const
     llvm::BasicBlock *BB = llvm::BasicBlock::Create(*TheContext, "entry", F);
     Builder->SetInsertPoint(BB);
 
-    std::vector<llvm::Type *> Args;
-
-    llvm::FunctionType *FT =
-        llvm::FunctionType::get(llvm::Type::getVoidTy(*TheContext), Args, false);
-
-    llvm::Function *F =
-        llvm::Function::Create(FT, llvm::Function::ExternalLinkage, 0, "main", TheModule);
-
-    llvm::BasicBlock *BB = llvm::BasicBlock::Create(*TheContext, "entry", F);
-    Builder->SetInsertPoint(BB);
-
-    ni::Context ctx(&llDest, TheContext, Builder);
+     ni::Context ctx(&llDest, TheContext, Builder);
 
     auto value = this->value->codegen(&ctx);
 
