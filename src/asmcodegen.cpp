e24d7fe95e349e4bbecfd82f659383e5cae7dbee
Filipe Constantinov Menezes <fcmenezes87@gmail.com>
2022-02-22 17:56:31 +0000

    Generate ASM code (#19)

---
+++ src/asmcodegen.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+
+#include "nodes.hh"
+#include "parser.tab.hh"
+
+int ni::ASMCodegen::internalCodegen(const ni::NProgram &node) {
+  *this->outputFile << "global _start" << std::endl;
+  *this->outputFile << std::endl;
+  *this->outputFile << "_start:" << std::endl;
+  *this->outputFile << "mov rax, 60" << std::endl;
+  *this->outputFile << "mov rdi, 100" << std::endl;
+  *this->outputFile << "syscall" << std::endl;
+  return 0;
+}
+
+int ni::ASMCodegen::codegen(const std::string &output, std::string &error)
+{
+  this->outputFile = new std::ofstream();
+  this->outputFile->open(output, std::ios::trunc);
+  ni::ASMCodegen::internalCodegen(this->program);
+  this->outputFile->close();
+  this->outputFile = nullptr;
+
+  return 0;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,27 @@
 #include <string>
 #include "nodes.hh"
 
+void printUsage(const std::string& app)
+{
+   std::cerr << "Usage: " << app << " <format> <srcfile> <destfile>" << std::endl;
+   std::cerr << "format: -ll for LLVM IR or -asm for Assembly" << std::endl;
+}
+
 int main(int argc, char *argv[])
 {
    ni::NProgram program;
    int op;
-   if (argc != 3)
+   if (argc != 4)
    {
-      std::cerr << "Usage: " << argv[0] << " <srcfile> <destfile>" << std::endl;
+      printUsage(argv[0]);
       exit(1);
    }
 
-   auto f = std::string(argv[1]);
+   auto format = std::string(argv[1]);
+   auto input = std::string(argv[2]);
+   auto output = std::string(argv[3]);
+
+   auto f = std::string(argv[2]);
    op = program.parse(f);
    if (op != 0)
    {
@@ -20,11 +30,29 @@ int main(int argc, char *argv[])
    }
 
    std::string error;
-   ni::LLVMCodegen c(program);
-   if (c.codegen(std::string(argv[2]), error) == 1)
+   ni::Codegen *c;
+   if (format.compare("-ll") == 0)
    {
+      c = new ni::LLVMCodegen(program);
+   }
+   else if (format.compare("-asm") == 0)
+   {
+      c = new ni::ASMCodegen(program);
+   }
+   else
+   {
+      std::cerr << "Error: invalid format"
+                << std::endl
+                << std::endl;
+      printUsage(argv[0]);
+      exit(1);
+   }
+   if (c->codegen(output, error) == 1)
+   {
+      delete c;
       std::cerr << error << std::endl;
       exit(1);
    }
+   delete c;
    return 0;
 }
