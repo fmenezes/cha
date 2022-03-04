390b8bb2ad30f51ec5718eb00be0a1419746d1ed
Filipe Constantinov Menezes <fcmenezes87@gmail.com>
2022-03-04 13:46:28 +0000

    Add args (#28)
    * Add args
    
    * Fix

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 result=0
+expected=200
 set -e
 ./a.out || result=$?
 set +e
-if [ $result -ne 200 ]; then
-    >&2 echo "error: expected 200 got $result"
+if [ $result -ne $expected ]; then
+    >&2 echo "error: expected $expected got $result"
     exit 1
 fi
