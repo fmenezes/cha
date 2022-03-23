737abcf43250f612ecd1acedac9c245f20b02305
Filipe Constantinov Menezes <fcmenezes87@gmail.com>
2022-03-23 17:48:29 +0000

    Add macOs tests

--- test/codegen/attprinter_test.cc
+++ test/codegen/attprinter_test.cc
@@ -84,6 +84,44 @@ TEST_F(ATTPrinterTest, Linux) {
   this->printer->ret();
   EXPECT_EQ(this->readFile(), expected);
 }
+
+TEST_F(ATTPrinterTest, MacOS) {
+  this->SetUp(
+      ni::codegen::Context(ni::codegen::OS::MACOS, ni::codegen::ARCH::x86_64));
+
+  std::string expected;
+  expected.append(".section\t__TEXT,__text\n");
+  expected.append(".globl\tstart\n");
+  expected.append("start:\n");
+  expected.append("\tcallq\t_test\n");
+  expected.append(".globl\t_test\n");
+  expected.append("_test:\n");
+  expected.append("\tmovq\t$10, %rax\n");
+  expected.append("\tmovl\t$10, %eax\n");
+  expected.append("\taddl\t$20, %edi\n");
+  expected.append("\tsubl\t$30, %ecx\n");
+  expected.append("\timull\t$40, %edx\n");
+  expected.append("\tpushq\t$50\n");
+  expected.append("\tsyscall\n");
+  expected.append("\tjmp\t_test2\n");
+  expected.append("\tretq\n");
+
+  this->printer->textHeader();
+  this->printer->labelStart();
+  this->printer->call("test");
+  this->printer->global("test");
+  this->printer->label("test");
+  this->printer->mov(ni::codegen::Register64Bits::RAX, 10);
+  this->printer->mov(ni::codegen::Register32Bits::EAX, 10);
+  this->printer->add(ni::codegen::Register32Bits::EDI, 20);
+  this->printer->sub(ni::codegen::Register32Bits::ECX, 30);
+  this->printer->imul(ni::codegen::Register32Bits::EDX, 40);
+  this->printer->push(50);
+  this->printer->syscall();
+  this->printer->jmp("test2");
+  this->printer->ret();
+  EXPECT_EQ(this->readFile(), expected);
+}
 } // namespace
 } // namespace test
 } // namespace ni
