a17bfbb01b4b126c3b6cb423ebb304eae33e40f5
Filipe Menezes <fcmenezes87@gmail.com>
2022-04-04 18:43:22 +0100

    Debug

--- test/ast/tokenizer_test.cpp
+++ test/ast/tokenizer_test.cpp
@@ -9,6 +9,8 @@ namespace test {
 namespace {
 
 TEST(TokenizerTest, Success) {
+  std::cout << "Debug Success\n";
+
   std::stringstream ss;
 
   ss << "fun main() int {\n";
@@ -17,6 +19,7 @@ TEST(TokenizerTest, Success) {
 
   ni::ast::tokenizer t("file.ni", ss);
 
+  std::cout << "Debug 2 Success\n";
 
   EXPECT_EQ(t.scan(), true);
   EXPECT_EQ(t.next().kind, ni::ast::token_kind::reserved_word);
@@ -54,23 +57,35 @@ TEST(TokenizerTest, Success) {
   EXPECT_EQ(t.next().kind, ni::ast::token_kind::symbol);
   EXPECT_EQ(t.next().text, "}");
 
+  std::cout << "Debug 3 Success\n";
+
   EXPECT_EQ(t.scan(), false);
   EXPECT_EQ(t.next().kind, ni::ast::token_kind::end_of_file);
+
+  std::cout << "Debug 4 Success\n";
 }
 
 TEST(TokenizerTest, UnknownChar) {
+  std::cout << "Debug UnknownChar\n";
+
   std::stringstream ss;
 
   ss << "fun main(%) int {\n";
   ss << "    ret 0\n";
   ss << "}\n";
 
+  std::cout << "Debug 2 UnknownChar\n";
   EXPECT_THROW({
+    std::cout << "Debug 3 UnknownChar\n";
     ni::ast::tokenizer t("file.ni", ss);
+    std::cout << "Debug 4 UnknownChar\n";
     while(t.scan()) {
+      std::cout << "Debug 5 UnknownChar\n";
       t.next();
     }
+    std::cout << "Debug 6 UnknownChar\n";
   }, ni::ast::tokenizer_error);
+  std::cout << "Debug 7 UnknownChar\n";
 }
 
 } // namespace
