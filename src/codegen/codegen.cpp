c9ede468182f94482b88a1f3446acbd2f288b019
Filipe Menezes <fcmenezes87@gmail.com>
2022-04-06 18:54:32 +0100

    fix

--- src/codegen/codegen.cpp
+++ src/codegen/codegen.cpp
@@ -9,7 +9,7 @@ ni::codegen::os ni::codegen::context::default_os() {
 #if defined(__APPLE__) || defined(__MACH__)
   return ni::codegen::os::MACOS;
 #else
-  return ni::codegen::OS::LINUX;
+  return ni::codegen::os::LINUX;
 #endif
 }
 
