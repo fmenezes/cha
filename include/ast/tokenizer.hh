b0009d6c29f9373e134deabafe7641ec46156eb9
Filipe Menezes <fcmenezes87@gmail.com>
2022-04-13 11:55:34 +0100

    Add input buffering for tokenizer

--- include/ast/tokenizer.hh
+++ include/ast/tokenizer.hh
@@ -30,15 +30,33 @@ public:
   }
 };
 
-enum token_kind { end_of_file, op, symbol, reserved_word, identifier, number };
+enum token_tag {
+  unknown,
+  end_of_file,
+  equals,
+  plus,
+  minus,
+  star,
+  open_parentheses,
+  close_parentheses,
+  open_braces,
+  close_braces,
+  comma,
+  fun,
+  ret,
+  var,
+  type_int,
+  identifier,
+  number
+};
 
 class token {
 public:
-  token(token_kind kind, const location &location)
-      : kind(kind), text(""), loc(location){};
-  token(token_kind kind, const std::string &text, const location &location)
-      : kind(kind), text(text), loc(location){};
-  token_kind kind;
+  token(token_tag tag, const location &location)
+      : tag(tag), text(""), loc(location){};
+  token(token_tag tag, const std::string &text, const location &location)
+      : tag(tag), text(text), loc(location){};
+  token_tag tag;
   std::string text;
   location loc;
 };
@@ -56,15 +74,27 @@ class tokenizer {
 public:
   tokenizer(const std::string &filename, std::istream *stream)
       : _stream(stream), _location(location(filename)),
-        _next(token(token_kind::end_of_file, location(filename))){};
+        _next(token(token_tag::end_of_file, location(filename))){};
   token scan_next_token();
   token last_token() const { return _next; };
 
 private:
-  char _c = -1;
+  char c[1024] = "";
+  std::streamsize c_len = 0;
+  short lexemeBegin = -1;
+  short lexemeForward = -1;
   token _next;
   location _location;
   std::istream *_stream;
+  bool read();
+  bool eof();
+  void peek();
+  void step();
+  void step_line();
+  void step_begin();
+  token set_token(token_tag tag);
+  token set_identifier_token();
+  std::string current_lexeme();
 };
 } // namespace ast
 } // namespace ni
--- test/ast/tokenizer_test.cpp
+++ test/ast/tokenizer_test.cpp
@@ -21,15 +21,14 @@ TEST(TokenizerTest, Success) {
   ni::ast::tokenizer t("file.ni", &ss);
 
   auto next = t.scan_next_token();
-  EXPECT_EQ(next.kind, ni::ast::token_kind::reserved_word);
-  EXPECT_EQ(next.text, "fun");
+  EXPECT_EQ(next.tag, ni::ast::token_tag::fun);
   EXPECT_EQ(next.loc.line_begin, 1);
   EXPECT_EQ(next.loc.line_end, 1);
   EXPECT_EQ(next.loc.column_begin, 1);
   EXPECT_EQ(next.loc.column_end, 4);
 
   next = t.scan_next_token();
-  EXPECT_EQ(next.kind, ni::ast::token_kind::identifier);
+  EXPECT_EQ(next.tag, ni::ast::token_tag::identifier);
   EXPECT_EQ(next.text, "main");
   EXPECT_EQ(next.loc.line_begin, 1);
   EXPECT_EQ(next.loc.line_end, 1);
@@ -37,47 +36,42 @@ TEST(TokenizerTest, Success) {
   EXPECT_EQ(next.loc.column_end, 9);
 
   next = t.scan_next_token();
-  EXPECT_EQ(next.kind, ni::ast::token_kind::symbol);
-  EXPECT_EQ(next.text, "(");
+  EXPECT_EQ(next.tag, ni::ast::token_tag::open_parentheses);
   EXPECT_EQ(next.loc.line_begin, 1);
   EXPECT_EQ(next.loc.line_end, 1);
   EXPECT_EQ(next.loc.column_begin, 9);
   EXPECT_EQ(next.loc.column_end, 10);
 
   next = t.scan_next_token();
-  EXPECT_EQ(next.kind, ni::ast::token_kind::symbol);
-  EXPECT_EQ(next.text, ")");
+  EXPECT_EQ(next.tag, ni::ast::token_tag::close_parentheses);
   EXPECT_EQ(next.loc.line_begin, 1);
   EXPECT_EQ(next.loc.line_end, 1);
   EXPECT_EQ(next.loc.column_begin, 10);
   EXPECT_EQ(next.loc.column_end, 11);
 
   next = t.scan_next_token();
-  EXPECT_EQ(next.kind, ni::ast::token_kind::reserved_word);
-  EXPECT_EQ(next.text, "int");
+  EXPECT_EQ(next.tag, ni::ast::token_tag::type_int);
   EXPECT_EQ(next.loc.line_begin, 1);
   EXPECT_EQ(next.loc.line_end, 1);
   EXPECT_EQ(next.loc.column_begin, 12);
   EXPECT_EQ(next.loc.column_end, 15);
 
   next = t.scan_next_token();
-  EXPECT_EQ(next.kind, ni::ast::token_kind::symbol);
-  EXPECT_EQ(next.text, "{");
+  EXPECT_EQ(next.tag, ni::ast::token_tag::open_braces);
   EXPECT_EQ(next.loc.line_begin, 1);
   EXPECT_EQ(next.loc.line_end, 1);
   EXPECT_EQ(next.loc.column_begin, 16);
   EXPECT_EQ(next.loc.column_end, 17);
 
   next = t.scan_next_token();
-  EXPECT_EQ(next.kind, ni::ast::token_kind::reserved_word);
-  EXPECT_EQ(next.text, "ret");
+  EXPECT_EQ(next.tag, ni::ast::token_tag::ret);
   EXPECT_EQ(next.loc.line_begin, 2);
   EXPECT_EQ(next.loc.line_end, 2);
   EXPECT_EQ(next.loc.column_begin, 5);
   EXPECT_EQ(next.loc.column_end, 8);
 
   next = t.scan_next_token();
-  EXPECT_EQ(next.kind, ni::ast::token_kind::number);
+  EXPECT_EQ(next.tag, ni::ast::token_tag::number);
   EXPECT_EQ(next.text, "0");
   EXPECT_EQ(next.loc.line_begin, 2);
   EXPECT_EQ(next.loc.line_end, 2);
@@ -85,18 +79,17 @@ TEST(TokenizerTest, Success) {
   EXPECT_EQ(next.loc.column_end, 10);
 
   next = t.scan_next_token();
-  EXPECT_EQ(next.kind, ni::ast::token_kind::symbol);
-  EXPECT_EQ(next.text, "}");
+  EXPECT_EQ(next.tag, ni::ast::token_tag::close_braces);
   EXPECT_EQ(next.loc.line_begin, 3);
   EXPECT_EQ(next.loc.line_end, 3);
   EXPECT_EQ(next.loc.column_begin, 1);
   EXPECT_EQ(next.loc.column_end, 2);
 
   next = t.scan_next_token();
-  EXPECT_EQ(next.kind, ni::ast::token_kind::end_of_file);
+  EXPECT_EQ(next.tag, ni::ast::token_tag::end_of_file);
 
   next = t.scan_next_token();
-  EXPECT_EQ(next.kind, ni::ast::token_kind::end_of_file);
+  EXPECT_EQ(next.tag, ni::ast::token_tag::end_of_file);
 }
 
 TEST(TokenizerTest, UnknownChar) {
@@ -106,11 +99,38 @@ TEST(TokenizerTest, UnknownChar) {
   s += "    ret 0\n";
   s += "}\n";
 
-  EXPECT_THROW({
-    std::istringstream ss(s);
-    ni::ast::tokenizer t("file.ni", &ss);
-    while(t.scan_next_token().kind != ni::ast::token_kind::end_of_file) {};
-  }, ni::ast::tokenizer_error);
+  std::istringstream ss(s);
+  ni::ast::tokenizer t("file.ni", &ss);
+
+ auto next = t.scan_next_token();
+  EXPECT_EQ(next.tag, ni::ast::token_tag::fun);
+  EXPECT_EQ(next.loc.line_begin, 1);
+  EXPECT_EQ(next.loc.line_end, 1);
+  EXPECT_EQ(next.loc.column_begin, 1);
+  EXPECT_EQ(next.loc.column_end, 4);
+
+  next = t.scan_next_token();
+  EXPECT_EQ(next.tag, ni::ast::token_tag::identifier);
+  EXPECT_EQ(next.text, "main");
+  EXPECT_EQ(next.loc.line_begin, 1);
+  EXPECT_EQ(next.loc.line_end, 1);
+  EXPECT_EQ(next.loc.column_begin, 5);
+  EXPECT_EQ(next.loc.column_end, 9);
+
+  next = t.scan_next_token();
+  EXPECT_EQ(next.tag, ni::ast::token_tag::open_parentheses);
+  EXPECT_EQ(next.loc.line_begin, 1);
+  EXPECT_EQ(next.loc.line_end, 1);
+  EXPECT_EQ(next.loc.column_begin, 9);
+  EXPECT_EQ(next.loc.column_end, 10);
+
+  next = t.scan_next_token();
+  EXPECT_EQ(next.tag, ni::ast::token_tag::unknown);
+  EXPECT_EQ(next.text, "%");
+  EXPECT_EQ(next.loc.line_begin, 1);
+  EXPECT_EQ(next.loc.line_end, 1);
+  EXPECT_EQ(next.loc.column_begin, 10);
+  EXPECT_EQ(next.loc.column_end, 11);
 }
 
 } // namespace
