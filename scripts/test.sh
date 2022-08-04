f869c2de6ea117b69506e9e0242daf3baf103cea
Filipe Constantinov Menezes <fcmenezes87@gmail.com>
2022-08-04 16:03:58 +0100

    Link Binaries (#90)

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,14 +3,9 @@
 result=0
 tmp="$(mktemp -d)"
 trap 'rm -rf "$tmp"' exit
-build/ni -c "$tmp/output.o" examples/test.ni
-if [[ $OSTYPE == 'darwin'* ]]; then
-    cc "$tmp/output.o" -o "$tmp/a.out" -lSystem
-else
-    cc "$tmp/output.o" -o "$tmp/a.out"
-fi
+build/ni -o "$tmp/test" examples/test.ni
 set -e
-"$tmp/a.out" || result=$?
+"$tmp/test" || result=$?
 set +e
 if [ $result -ne 0 ]; then
     >&2 echo "error: expected 0 got $result"
