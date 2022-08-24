ea5e24df6a33b3771d9955e56f79e0229df60224
Filipe Menezes <fcmenezes87@gmail.com>
2022-08-24 17:05:28 +0100

    Change clang-format

--- nic/src/codegen.c
+++ nic/src/codegen.c
@@ -27,7 +27,6 @@ LLVMValueRef return_operand = NULL;
 symbol_table *var_table = NULL;
 symbol_table *fn_table = NULL;
 
-
 void ni_ast_codegen_node(ni_ast_node *ast_node) {
   return_operand = NULL;
   if (ast_node == NULL) {
@@ -106,17 +105,17 @@ void ni_ast_codegen_node_bin_op(ni_ast_node *ast_node) {
 
 void ni_ast_codegen_node_var(ni_ast_node *ast_node) {
   LLVMTypeRef type = LLVMInt32Type();
-  
-  LLVMValueRef addr = LLVMBuildAlloca(
-      builder, type, ast_node->variable_declaration.identifier);
-  
+
+  LLVMValueRef addr =
+      LLVMBuildAlloca(builder, type, ast_node->variable_declaration.identifier);
+
   symbol_value *value = malloc(sizeof(symbol_value));
   value->type = type;
   value->ref = addr;
   value->node = ast_node;
 
   insert_symbol_table(var_table, ast_node->variable_declaration.identifier,
-                    value);
+                      value);
 }
 
 void ni_ast_codegen_node_var_assign(ni_ast_node *ast_node) {
@@ -129,7 +128,7 @@ void ni_ast_codegen_node_var_assign(ni_ast_node *ast_node) {
 void ni_ast_codegen_node_var_lookup(ni_ast_node *ast_node) {
   symbol_value *value =
       get_symbol_table(var_table, ast_node->variable_lookup.identifier);
-  return_operand = LLVMBuildLoad2(builder, value->type, value->ref, 
+  return_operand = LLVMBuildLoad2(builder, value->type, value->ref,
                                   ast_node->variable_lookup.identifier);
 }
 
@@ -139,8 +138,9 @@ void ni_ast_codegen_node_call(ni_ast_node *ast_node) {
   if (ast_node->function_call.argument_list != NULL) {
     arg_count = ast_node->function_call.argument_list->count;
     args = malloc(sizeof(LLVMValueRef) * arg_count);
-    
-    ni_ast_node_list_entry *current = ast_node->function_call.argument_list->head;
+
+    ni_ast_node_list_entry *current =
+        ast_node->function_call.argument_list->head;
     int i = 0;
     while (current != NULL) {
       ni_ast_codegen_node(current->node);
@@ -150,7 +150,8 @@ void ni_ast_codegen_node_call(ni_ast_node *ast_node) {
     }
   }
 
-  symbol_value *function = get_symbol_table(fn_table, ast_node->function_call.identifier);
+  symbol_value *function =
+      get_symbol_table(fn_table, ast_node->function_call.identifier);
 
   return_operand =
       LLVMBuildCall2(builder, function->type, function->ref, args, arg_count,
@@ -174,18 +175,20 @@ void ni_ast_codegen_node_fun(ni_ast_node *ast_node) {
   entry->ref = function;
   entry->type = fn_type;
 
-  insert_symbol_table(fn_table, ast_node->function_declaration.identifier, entry);
+  insert_symbol_table(fn_table, ast_node->function_declaration.identifier,
+                      entry);
 
   if (ast_node->function_declaration.argument_list != NULL) {
     LLVMBasicBlockRef args_block = LLVMAppendBasicBlock(function, "args");
     LLVMPositionBuilderAtEnd(builder, args_block);
 
-    ni_ast_node_list_entry *current = ast_node->function_declaration.argument_list->head;
+    ni_ast_node_list_entry *current =
+        ast_node->function_declaration.argument_list->head;
     int i = 0;
     while (current != NULL) {
       LLVMTypeRef type = LLVMInt32Type();
-      LLVMValueRef addr = LLVMBuildAlloca(
-          builder, type, current->node->argument.identifier);
+      LLVMValueRef addr =
+          LLVMBuildAlloca(builder, type, current->node->argument.identifier);
       LLVMBuildStore(builder, LLVMGetParam(function, i), addr);
 
       symbol_value *value = malloc(sizeof(symbol_value));
@@ -235,7 +238,7 @@ void ni_ast_codegen(ni_ast_node_list *ast, enum ni_ast_codegen_format format,
 LLVMTypeRef make_fun_signature(ni_ast_node *ast_node) {
   LLVMTypeRef *arg_types = NULL;
   int arg_count = 0;
-  if (ast_node->function_declaration.argument_list != NULL){
+  if (ast_node->function_declaration.argument_list != NULL) {
     arg_count = ast_node->function_declaration.argument_list->count;
   }
   if (arg_count > 0) {
