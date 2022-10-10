07be944389f3e20192a4b2c5f0ffaea2f0cfebae
Filipe Menezes <fcmenezes87@gmail.com>
2022-10-10 11:46:48 +0100

    Assert error messages

--- test/test.c
+++ test/test.c
@@ -2,72 +2,119 @@
 #include <stdlib.h>
 #include <string.h>
 
+void cleanup();
+int run_process(char *cmd, char **output);
+void check_success(const char* file);
+void check_failure(const char *file, const char *msg);
+
 void cleanup() { remove("out.ll"); }
 
-int check_for_failure(const char *filePath) {
-  char suffix[] = "_passes.ni";
-  if (strlen(filePath) < strlen(suffix)) {
-    return 1;
+void check_failure(const char *file, const char* msg) {
+  char cmd[1000];
+  char *got_msg = NULL;
+  sprintf(cmd, "ni -ll out.ll %s 2>&1", file);
+  int ret = run_process(cmd, &got_msg);
+  cleanup();
+  if (ret == 0) {
+    fprintf(stderr, "\"%s\" returned 0 expected non-zero\n", cmd);
+    exit(1);
   }
-
-  if (strcmp(filePath + strlen(filePath) - strlen(suffix), suffix) == 0) {
-    return 0;
+  if (strstr(got_msg, msg) == NULL) {
+    fprintf(stderr, "expected: \"%s\", got : \"%s\"\n", msg, got_msg);
+    free(got_msg);
+    exit(1);
   }
-
-  return 1;
+  free(got_msg);
+  exit(0);
 }
 
 int run_process(char *cmd, char **output) {
   char buffer[10000];
   FILE *data = popen(cmd, "r");
   if (data == NULL) {
-    sprintf(*output, "no pipe\n");
+    if (output != NULL) {
+      sprintf(*output, "no pipe\n");
+    }
     return -1;
   }
-  if (fgets(buffer, 10000, data) != NULL) {
+  if (output != NULL && fgets(buffer, 10000, data) != NULL) {
     *output = strdup(buffer);
   }
   return pclose(data);
 }
 
-int main(int argc, char *argv[]) {
-  if (argc != 3) {
-    fprintf(stderr, "Usage: %s <path/to/nic> <test>\n", argv[0]);
-    return 1;
-  }
-
-  char *nicPath = argv[1];
-  char *inputPath = argv[2];
+void check_success(const char* file) {
   char cmd[1000];
-  char *msg_from_cmd = NULL;
-  int ret = 0;
-  FILE *file = NULL;
-  FILE *data = NULL;
+  sprintf(cmd, "ni -ll out.ll %s 2>&1", file);
+  int ret = run_process(cmd, NULL);
+  cleanup();
+  if (ret != 0) {
+    fprintf(stderr, "\"%s\" returned %d expected 0\n", cmd, ret);
+    exit(1);
+  }
+  exit(0);
+}
 
-  if ((file = fopen(inputPath, "r")) != NULL) {
-    fclose(file);
-  } else {
-    fprintf(stderr, "could not find %s\n", inputPath);
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s <test>\n", argv[0]);
     return 1;
   }
 
-  sprintf(cmd, "%s -ll out.ll %s 2>&1", nicPath, inputPath);
-  ret = run_process(cmd, &msg_from_cmd);
-  cleanup();
-
-  fprintf(stderr, "got: %s", msg_from_cmd);
-  free(msg_from_cmd);
-  if (check_for_failure(inputPath)) {
-    if (ret == 0) {
-      fprintf(stderr, "\"%s\" returned %d expected non zero\n", cmd, ret);
-      return 1;
-    }
-  } else {
-    if (ret != 0) {
-      fprintf(stderr, "\"%s\" returned %d expected 0\n", cmd, ret);
-      return 1;
-    }
+  char *inputPath = argv[1];
+  
+  if (strstr(inputPath, "parse_passes.ni") != NULL) {
+    check_success(inputPath);
   }
 
-  return 0;
+  if (strstr(inputPath, "parse_failure.ni") != NULL) {
+    check_failure(inputPath, "syntax error");
+  }
+
+  if (strstr(inputPath, "validation_passes.ni") != NULL) {
+    check_success(inputPath);
+  }
+
+  if (strstr(inputPath, "validation_arg_dup.ni") != NULL) {
+    check_failure(inputPath, "argument 'i' already defined");
+  }
+
+  if (strstr(inputPath, "validation_arg_mismatch.ni") != NULL) {
+    check_failure(inputPath, "function 'test' expects no arguments");
+  }
+
+  if (strstr(inputPath, "validation_arg_mismatch2.ni") != NULL) {
+    check_failure(inputPath, "function 'test' expects arguments");
+  }
+
+  if (strstr(inputPath, "validation_arg_mismatch3.ni") != NULL) {
+    check_failure(inputPath, "type mismatch!!!");
+  }
+
+  if (strstr(inputPath, "validation_dup_function.ni") != NULL) {
+    check_failure(inputPath, "function 'test' already defined");
+  }
+
+  if (strstr(inputPath, "validation_function_not_found.ni") != NULL) {
+    check_failure(inputPath, "function 'test' not found");
+  }
+
+  if (strstr(inputPath, "validation_type_mismatch.ni") != NULL) {
+    check_failure(inputPath, "type mismatch!!!");
+  }
+
+  if (strstr(inputPath, "validation_var_not_found.ni") != NULL) {
+    check_failure(inputPath, "variable 'a' not found");
+  }
+
+  if (strstr(inputPath, "validation_var_redefined.ni") != NULL) {
+    check_failure(inputPath, "variable 'a' already defined");
+  }
+
+  if (strstr(inputPath, "validation_var_redefined2.ni") != NULL) {
+    check_failure(inputPath, "variable 'a' already defined");
+  }
+
+  fprintf(stderr, "\"%s\" is unknown\n", inputPath);
+  return 1;
 }
